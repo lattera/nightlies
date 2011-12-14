f779f45be8be19cbf7c7db153e19837bff457deb
Shawn Webb <lattera@gmail.com>
2011-12-14 09:54:48 -0700

    Fix a few bugs

--- run.sh
+++ run.sh
@@ -6,27 +6,27 @@ debugging="false"
 
 cd `dirname "$0"`
 
-if [ -f "env.txt"  ]
-then
+if [ -f "env.txt"  ]; then
     env=`cat env.txt`
 else
     echo "Please place environment in env.txt"
     exit 0
 fi
 
-if [ -f "debug" ]
-then
+if [ -f "debug" ]; then
     debugging="true"
 fi
 
 for line in `find $env -name nightlie.sh`; do
-    # Use one log file per nightly
-    logfile=`echo $line | sed 's/\//_/g'`
-    exec 1> logs/$date-$logfile.log
-    exec 2>&1
+    if [ "$debugging" == "false" ]; then
+        # Use one log file per nightly
+        logfile=`echo $line | sed 's/\//_/g'`
+        exec 1> logs/$date-$logfile.log
+        exec 2>&1
+    fi
 
     echo "[+] Executing $line"
-    if [ "$debugging" = "false" ]; then
-        exec $line
+    if [ "$debugging" == "false" ]; then
+        $line
     fi
 done
