95b652f2e4a39800f72bd8130e6e4d3d4733614a
Shawn Webb <lattera@gmail.com>
2012-04-23 15:24:36 -0600

    Force building world and kernel

--- freebsd/freebsd-release/nightlie.sh
+++ freebsd/freebsd-release/nightlie.sh
@@ -1,5 +1,13 @@
 #!/usr/local/bin/bash
 
+KERNCONF=SEC
+
+set -e
 cd /usr/src/release
+
+cd /usr/src
+make -j4 buildworld
+make -j4 buildkernel KERNCONF=${KERNCONF}
+
 sudo make clean
-sudo make NOPORTS=1 NODOC=1 NOSRC=1 KERNCONF=SEC MAKE_FLAGS=-j4 release
+sudo make NOPORTS=1 NODOC=1 NOSRC=1 KERNCONF=${KERNCONF} MAKE_FLAGS=-j4 release
