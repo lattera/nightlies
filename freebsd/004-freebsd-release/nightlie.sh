79f83edff4cc44aa3127a1241603acbd97189cf5
Shawn Webb <lattera@gmail.com>
2012-04-24 09:58:54 -0600

    Properly make the release

--- freebsd/004-freebsd-release/nightlie.sh
+++ freebsd/004-freebsd-release/nightlie.sh
@@ -3,11 +3,11 @@
 KERNCONF=SEC
 
 set -e
-cd /usr/src/release
 
 cd /usr/src
 make -j4 buildworld
 make -j4 buildkernel KERNCONF=${KERNCONF}
 
+cd /usr/src/release
 sudo make clean
 sudo make NOPORTS=1 NODOC=1 NOSRC=1 KERNCONF=${KERNCONF} MAKE_FLAGS=-j4 release
