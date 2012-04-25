5108e5b1a9d0ac62aebe86f8d125695a85c2a9cd
Shawn Webb <lattera@gmail.com>
2012-04-25 14:16:07 -0600

    Push nightly builds to my web server

--- freebsd/004-freebsd-release/nightlie.sh
+++ freebsd/004-freebsd-release/nightlie.sh
@@ -2,6 +2,8 @@
 
 KERNCONF=SEC
 
+DATE=`date '+%F'`
+
 set -e
 
 cd /usr/src
@@ -11,3 +13,6 @@ make -j4 buildkernel KERNCONF=${KERNCONF}
 cd /usr/src/release
 sudo make clean
 sudo make NOPORTS=1 NODOC=1 NOSRC=1 KERNCONF=${KERNCONF} MAKE_FLAGS=-j4 release
+
+scp /usr/obj/src/release/release.iso www.0xfeedface.org:public_html/freebsd/nightlies/9-stable/${DATE}.iso
+scp /usr/obj/src/release/memstick www.0xfeedface.org:public_html/freebsd/nightlies/9-stable/${DATE}.memstick.img
