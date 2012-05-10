c9449e861748501fa489be83a231f2a8e66f531b
Shawn Webb <lattera@gmail.com>
2012-05-10 09:00:01 -0600

    Add BHyVe to nightly scripts. Change freebsd-src nightly to key off ZFS.

--- freebsd/005-bhyve/nightlie.sh
+++ freebsd/005-bhyve/nightlie.sh
@@ -1,12 +1,8 @@
 #!/usr/local/bin/bash
 
-dataset="rpool/src/freebsd"
-branch="svn_stable_9"
-
+dataset="rpool/src/bhyve"
 directory=$(zfs get -H -ovalue mountpoint ${dataset})
 
 cd ${directory}
-
-git fetch upstream
-git merge upstream/${branch}
+git svn rebase
 git push github
