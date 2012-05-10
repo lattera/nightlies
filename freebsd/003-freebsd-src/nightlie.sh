7d9192763aeaeff5e9c34f8496eb6fede8287f64
Shawn Webb <lattera@gmail.com>
2012-05-10 09:16:26 -0600

    Add syncing freebsd master branch (-current)

--- freebsd/003-freebsd-src/nightlie.sh
+++ freebsd/003-freebsd-src/nightlie.sh
@@ -7,6 +7,15 @@ directory=$(zfs get -H -ovalue mountpoint ${dataset})
 
 cd ${directory}
 
+curbranch=$(git branch | grep '*' | awk '{print $2;}')
+
 git fetch upstream
-git merge upstream/${branch}
+git merge upstream/${curbranch}
+
+if [ ! "${curbranch}" == "master" ]; then
+    git checkout master
+    git merge upstream/master
+    git checkout ${curbranch}
+fi
+
 git push github
